4828915a8ad601053e690cb936d2be2f4216192d
Kevin Kam <k040691@icloud.com>
2018-12-26 21:55:59 +0800

    added can change player position feature

--- src/actions/PlayersActions.js
+++ src/actions/PlayersActions.js
@@ -1,22 +1,23 @@
-import * as types from '../constants/ActionTypes';
+import * as types from "../constants/ActionTypes";
 
-export function addPlayer(name) {
+export function addPlayer(name, position) {
   return {
     type: types.ADD_PLAYER,
     name,
+    position
   };
 }
 
 export function deletePlayer(id) {
   return {
     type: types.DELETE_PLAYER,
-    id,
+    id
   };
 }
 
 export function starPlayer(id) {
   return {
     type: types.STAR_PLAYER,
-    id,
+    id
   };
 }
