5927e0d141f9dfd81c88b2e7bd8050d50bf1ee6e
frank zhao <zhy20090912@gmail.com>
2018-12-27 22:32:03 +0800

    add Pagination

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -2,25 +2,43 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './PlayerList.css';
 import PlayerListItem from './PlayerListItem';
+import Pagination from './Pagination';
 
 class PlayerList extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+        pageOfItems: []
+    };
+
+    this.onChangePage = this.onChangePage.bind(this);
+  }
+  
+  onChangePage(pageOfItems) {
+    this.setState({ pageOfItems: pageOfItems });
+  }
+
   render() {
     return (
-      <ul className={styles.playerList}>
-        {this.props.players.map((player, index) => {
-          return (
-            <PlayerListItem
-              key={index}
-              id={index}
-              name={player.name}
-              team={player.team}
-              position={player.position}
-              starred={player.starred}
-              {...this.props.actions}
-            />
-          );
-        })}
-      </ul>
+      <div>
+        <ul className={styles.playerList}>
+          {this.state.pageOfItems.map((player, index) => {
+            return (
+              <PlayerListItem
+                key={index}
+                id={index}
+                name={player.name}
+                team={player.team}
+                position={player.position}
+                starred={player.starred}
+                {...this.props.actions}
+              />
+            );
+          })}
+        </ul>
+        <Pagination items={this.props.players} onChangePage={this.onChangePage} />
+      </div>
     );
   }
 }
